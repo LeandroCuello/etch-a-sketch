c630839030d684e5d3bc70c37ff04acb04801f42
Leando Cuello <leandroezequiel1999@gmail.com>
2020-09-27 18:51:56 -0300

    added color options, a erase, and a way to change the cavas size white bottuns and input

--- code.js
+++ code.js
@@ -1,15 +1,74 @@
+//setting DOMS elements as variables
+const gridContainer = document.querySelector('#gridContainer');
+const cellManageDisplay = document.querySelector('#cm_display');
+const cellManageAddBtn = document.querySelector('#cm_more');
+const cellManageRestBtn = document.querySelector('#cm_less');
+const blackSelector = document.querySelector('#black_selector');
+const whiteSelector = document.querySelector('#white_selector');
+const colorsSelector = document.querySelector('#colors_selector');
+const eraseButton = document.querySelector('#erase');
+
+//Array with color options
+const colorsOptions = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+//defaults setting for when the browser is open.
+gridCreator(+cellManageDisplay.getAttribute('placeholder'));
+let colorSelected = 'black';
+//logic for the bottuns to add or rest from the display
+cellManageAddBtn.addEventListener('click', () => botttunPress(1));
+cellManageRestBtn.addEventListener('click', () => botttunPress(-1))
+
+function botttunPress (addedNumber){
+    let tmpNumber = +cellManageDisplay.value;
+    tmpNumber += addedNumber;    
+    gridCreator(tmpNumber);
+}
+//logic for erase button
+eraseButton.addEventListener('click', createGrid);
+
+//logic for the color select bottuns
+blackSelector.addEventListener('click', () => colorSelector("black"));
+whiteSelector.addEventListener('click', () => colorSelector("white"));
+colorsSelector.addEventListener('click', () => colorSelector("colors"));
+
+function colorSelector(color){
+
+    blackSelector.classList.remove('press_color_selector');
+    whiteSelector.classList.remove('press_color_selector');
+    colorsSelector.classList.remove('press_color_selector');
+
+    if(color == "black"){
+        blackSelector.classList.add('press_color_selector');
+    }else if(color == "white"){
+        whiteSelector.classList.add('press_color_selector');
+    }else{
+        colorsSelector.classList.add('press_color_selector');
+    }
+    
+    colorSelected = color;
+}
+
+//add events listener to notice a change in the display that shows the amount of
+//colums the grid has.
+cellManageDisplay.addEventListener('change', createGrid);
+
+function createGrid(){
+    gridCreator(cellManageDisplay.value);
+}
 
 //for creating the grid.
-gridCreator(16);
 function gridCreator(amountOfColums){
 
-    const gridContainer = document.querySelector('#gridContainer');
-
+    cellManageDisplay.value = amountOfColums;
+    amountOfColums = +amountOfColums;
     gridContainer.innerHTML = '';
 
-    if (typeof(amountOfColums)!="number" ||
-    amountOfColums<1||
-    amountOfColums>50) return 'ERROR';
+    if ( isNaN(amountOfColums) ||
+    amountOfColums<2||
+    amountOfColums>64){
+        amountOfColums = 2;
+        cellManageDisplay.value = 2;
+    }
 
     let amountOfrows = amountOfColums;
 
@@ -31,10 +90,19 @@ function gridCreator(amountOfColums){
 // created to create evente listeners after the grid is created or modified.
 function addEventListenerToCells(){
     const gridCells = Array.from(document.querySelectorAll(".grid_cell"));
-    gridCells.forEach(cell => cell.addEventListener('mouseover', PaintIblack));
+    gridCells.forEach(cell => cell.addEventListener('mouseover', painter));
 }
 
 //color fuctions
-function PaintIblack(e){
-    this.style.setProperty('background-color', 'black');
+function painter(){
+    if (colorSelected != "colors"){
+        this.style.setProperty('background-color', colorSelected);
+    }else{
+        this.style.setProperty('background-color', colorsOptions[getRandomInt(colorsOptions.length -1)])
+    }
+}
+
+//choose a random number
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
